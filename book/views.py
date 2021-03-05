cb8978b438cd7e9bf0c68195e19b13713c4b17f5
mhazad <nmzistg@gmail.com>
2021-03-06 02:38:04 +0330

    1-2 serializer created and rest_framework added +serializer setting:permissions added urls: router urls added views: viewsets for book user and author added

--- book/views.py
+++ book/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+# from permissions import NotCreateAndIsAdminUser
+# from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from book.models import Book, Author
+from book.serializers import BookSerializer, AuthorSerializer, UserSerializer
 
-# Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    http_method_names = ['get', 'post', 'put', 'delete', 'creat']
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    http_method_names = ['get', 'post', 'put', 'delete', 'creat']
+
+    # overwriting creat permission for Author
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'list']:
+            self.permission_classes = [permissions.IsAuthenticated, ]
+        elif self.action in ['create']:
+            self.permission_classes = [permissions.IsAdminUser, ]
+        else:
+            self.permission_classes = [permissions.AllowAny, ]
+        return super(self.__class__, self).get_permissions()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # overwriting creat permission for Author
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy', 'list']:
+            self.permission_classes = [permissions.IsAuthenticated, ]
+        elif self.action in ['create']:
+            self.permission_classes = [permissions.IsAdminUser, ]
+        else:
+            self.permission_classes = [permissions.AllowAny, ]
+        return super(self.__class__, self).get_permissions()
--- django_rest_challenge/settings.py
+++ django_rest_challenge/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
     'book'
 ]
 
@@ -115,3 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+        # 'rest_framework.permissions.IsAuthenticated'
+    ],
+}
